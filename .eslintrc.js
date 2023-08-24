bddbd77aa1e0ed55c47d72291af2e36a05759c25
Tomoaki Imai <tomoima525@gmail.com>
2023-08-23 18:58:27 -1000

    add better eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,48 @@ module.exorts = {
     jest: true,
     browser: true,
   },
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
-    sourceType: 'module',
+    project: "./tsconfig.json",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+    "typescript-sort-keys",
+    "unused-imports",
+  ],
   rules: {
-    'no-console': 0,
+    "unused-imports/no-unused-imports": "warn",
+    "import/no-cycle": ["warn"],
+
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", ["parent", "sibling"]],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "./**.scss",
+            group: "sibling",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "no-multiple-empty-lines": "warn",
     semi: 0,
-    '@typescript-eslint/no-explicit-any': 0,
+    "@typescript-eslint/no-explicit-any": 0,
   },
 };
